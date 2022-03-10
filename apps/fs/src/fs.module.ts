8471699843b8ac58fbd49df4dc628a0383079864
henry <cksgod16@gmail.com>
2022-03-10 20:28:49 +0900

    add: fs module

--- apps/fs/src/fs.module.ts
+++ apps/fs/src/fs.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { FsController } from './fs.controller';
-import { FsService } from './fs.service';
 
 @Module({
   imports: [],
-  controllers: [FsController],
-  providers: [FsService],
+  controllers: [],
+  providers: [],
 })
 export class FsModule {}
