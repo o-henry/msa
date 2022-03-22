fe46dfc163209b4ea96c0dd4e9d240a71196ebc3
henry <cksgod16@gmail.com>
2022-03-22 12:49:32 +0900

    update: order entity with annotation

---
+++ apps/order/vo/item.vo.ts
@@ -0,0 +1,28 @@
+import { Result } from '@badrap/result';
+
+interface ItemProps {
+  quantity: number;
+  name: string;
+}
+
+export class Item implements ItemProps {
+  protected readonly props: ItemProps;
+
+  constructor(props) {}
+
+  public get quantity(): number {
+    return this.props.quantity;
+  }
+
+  public get name(): string {
+    return this.props.name;
+  }
+
+  protected validate(props: ItemProps): Result<Item> {
+    if (props.quantity < 0) {
+      return Result.err(new Error('quantity is out of range'));
+    }
+
+    return Result.ok(new Item(props));
+  }
+}
